120452d8e75ec973c0b7cb55d6b4b095b882cd12
hebingchang <hebingchang1@live.com>
2018-07-31 09:39:05 +0800

    Fix a bug

--- src/views/records/index.vue
+++ src/views/records/index.vue
@@ -1,80 +1,82 @@
 <template>
-  <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>签到记录</span>
-    </div>
-    <div class="text item">
-      <el-form ref="form" :inline="true" :model="form" label-width="80px">
-        <el-form-item label="百度账号">
-          <el-select v-model="bduss_id" placeholder="请选择账户" @change="refresh">
-            <el-option :label="item.baidu_name" :value="item.id" v-for="item in bdusses" :key="item.id"></el-option>
-          </el-select>
-        </el-form-item>
+  <div class="app-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>签到记录</span>
+      </div>
+      <div class="text item">
+        <el-form ref="form" :inline="true" :model="form" label-width="80px">
+          <el-form-item label="百度账号">
+            <el-select v-model="bduss_id" placeholder="请选择账户" @change="refresh">
+              <el-option :label="item.baidu_name" :value="item.id" v-for="item in bdusses" :key="item.id"></el-option>
+            </el-select>
+          </el-form-item>
 
-        <el-form-item label="日期">
-          <el-date-picker
-            v-model="date"
-            value-format="yyyy-MM-dd"
-            align="right"
-            type="date"
-            placeholder="选择日期"
-            :picker-options="pickerOptions1"
-            @change="refresh">
-          </el-date-picker>
-        </el-form-item>
+          <el-form-item label="日期">
+            <el-date-picker
+              v-model="date"
+              value-format="yyyy-MM-dd"
+              align="right"
+              type="date"
+              placeholder="选择日期"
+              :picker-options="pickerOptions1"
+              @change="refresh">
+            </el-date-picker>
+          </el-form-item>
 
-      </el-form>
+        </el-form>
 
-      <el-form ref="form">
-        <el-form-item v-if="bduss_id != null && !signing">
-          <el-button type="primary" @click="sign">立即签到</el-button>
-        </el-form-item>
-        <el-form-item :label="signing_status" v-if="signing">
-          <el-progress :text-inside="true" :stroke-width="18" :percentage="progress"></el-progress>
-        </el-form-item>
-      </el-form>
+        <el-form ref="form">
+          <el-form-item v-if="bduss_id != null && !signing">
+            <el-button type="primary" @click="sign">立即签到</el-button>
+          </el-form-item>
+          <el-form-item :label="signing_status" v-if="signing">
+            <el-progress :text-inside="true" :stroke-width="18" :percentage="progress"></el-progress>
+          </el-form-item>
+        </el-form>
 
 
-      <el-table
-        :data="records"
-        style="width: 100%"
-        :default-sort = "{prop: 'sign_history', order: 'descending'}"
-        v-if="!signing"
-      >
+        <el-table
+          :data="records"
+          style="width: 100%"
+          :default-sort = "{prop: 'sign_history', order: 'descending'}"
+          v-if="!signing"
+        >
 
-        <el-table-column
-          label="贴吧名称"
-          sortable
-          fixed>
-          <template slot-scope="scope">
-            <a :href="'http://tieba.baidu.com/f?kw=' + scope.row.forum_name" style="text-decoration: none;" class="el-button el-button--text" target="_blank"><span>{{ scope.row.forum_name }}</span></a>
-          </template>
-        </el-table-column>
+          <el-table-column
+            label="贴吧名称"
+            sortable
+            fixed>
+            <template slot-scope="scope">
+              <a :href="'http://tieba.baidu.com/f?kw=' + scope.row.forum_name" style="text-decoration: none;" class="el-button el-button--text" target="_blank"><span>{{ scope.row.forum_name }}</span></a>
+            </template>
+          </el-table-column>
 
-        <el-table-column
-          prop="level_name"
-          label="等级"
-          sortable>
-        </el-table-column>
+          <el-table-column
+            prop="level_name"
+            label="等级"
+            sortable>
+          </el-table-column>
 
-        <el-table-column
-          prop="cur_score"
-          label="当前积分"
-          sortable>
-        </el-table-column>
+          <el-table-column
+            prop="cur_score"
+            label="当前积分"
+            sortable>
+          </el-table-column>
 
-        <el-table-column
-          label="签到情况"
-          sortable>
-          <template slot-scope="scope">
-            {{ scope.row.sign_history.has_signed ? "已签到" : (scope.row.sign_history.error_msg ? "签到失败 ("+scope.row.sign_history.error_msg+")" : "未签到") }}
-          </template>
-        </el-table-column>
+          <el-table-column
+            label="签到情况"
+            sortable>
+            <template slot-scope="scope">
+              {{ scope.row.sign_history.has_signed ? "已签到" : (scope.row.sign_history.error_msg ? "签到失败 ("+scope.row.sign_history.error_msg+")" : "未签到") }}
+            </template>
+          </el-table-column>
 
-      </el-table>
+        </el-table>
 
-    </div>
-  </el-card>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
